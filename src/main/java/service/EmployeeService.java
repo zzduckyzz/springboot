d0cd24cf79dd6910439035a7cebb650e5bea5884
Duc Pham <ducpmth1903004@fpt.edu.vn>
2021-06-17 01:30:37 +0700

    spring

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package service;
+
+import entity.Employee;
+
+import java.util.List;
+
+
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
+}
